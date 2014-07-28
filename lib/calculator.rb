b8e9b5fc800f561c7872b54b2dd66fa62efa77cf
Meghan Lindsley & Aoede Pando <student@epicodus.com>
2014-07-28 10:48:12 -0700

    second commit. takes a subtraction problem and returns a number

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,12 +1,12 @@
 def calculator(string)
   string.gsub! 'plus', '+'
-  # string.gsub! 'minus', '-'
+  string.gsub! 'minus', '-'
   array = string.split('')
   new_string = array.slice(8..-2).join('')
   answer = eval(new_string)
   p answer
 end
 
-calculator('What is 10 plus 3?')
+calculator('What is 10 minus 3?')
 
 
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -6,7 +6,7 @@ describe('calculator') do
     calculator('What is 5 plus 3?').should(eq(8))
   end
 
-  # it('takes a subtraction word equation and returns the answer') do
-  #   calculator('What is 10 minus 3').should(eq(7))
-  # end
+  it('takes a subtraction word equation and returns the answer') do
+    calculator('What is 10 minus 3?').should(eq(7))
+  end
 end
