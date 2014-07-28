0ce3f14ed50bc5ceab001757f1f4926049c5d8dd
Meghan Lindsley & Aoede Pando <student@epicodus.com>
2014-07-28 14:21:48 -0700

    seventh commit, takes a string with two operators and returns an answer.

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,5 +1,6 @@
+require 'pry'
+
 def calculator(string)
-  # array = string.split('?')
   string.gsub! 'plus', '+'
   string.gsub! 'minus', '-'
   string.gsub! 'times', '*'
@@ -8,13 +9,25 @@ def calculator(string)
   string.gsub! 'th power', ''
   string.gsub! 'nd power', ''
   string.gsub! 'to the', '**'
-  array = string.split('')
-  new_string = array.slice(8..-2).join('')
-  answer = eval(new_string)
-  p answer
+  array = string.split('? ')
 
+  answer =[]
+  if (array.count == 2)
+    string1 = array[0]
+    string2 = array[1]
+
+    first_string = string1.slice(8..-1)
+    second_string = string2.slice(8..-2)
+    answer << eval(first_string)
+    answer << eval(second_string)
+  else
+    one_string = array[0].slice(8..-2)
+    p one_string
+    answer << eval(one_string)
+  end
+
+  answer
 end
 
-calculator('What is 4 plus 6 divided by 2?')
 
-
+calculator('What is 4 plus 2? What is 12 / 3?')
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -3,26 +3,30 @@ require('calculator')
 
 describe('calculator') do
   it('takes an addition word equation and returns the answer') do
-    calculator('What is 5 plus 3?').should(eq(8))
+    calculator('What is 5 plus 3?').should(eq([8]))
   end
 
   it('takes a subtraction word equation and returns the answer') do
-    calculator('What is 10 minus 3?').should(eq(7))
+    calculator('What is 10 minus 3?').should(eq([7]))
   end
 
   it('takes a multiplication word equation and returns the answer') do
-    calculator('What is 3 times 4?').should(eq(12))
+    calculator('What is 3 times 4?').should(eq([12]))
   end
 
   it('takes a division word equation and returns the answer') do
-    calculator('What is 12 divided by 3?').should(eq(4))
+    calculator('What is 12 divided by 3?').should(eq([4]))
   end
 
   it('takes an exponential word equation and returns the answer') do
-    calculator('What is 2 to the 3rd power?').should(eq(8))
+    calculator('What is 2 to the 3rd power?').should(eq([8]))
   end
 
   it('takes an equation with multiple operators and returns the answer') do
-    calculator('What is 4 plus 6 divided by 2?').should(eq(7))
+    calculator('What is 4 plus 6 divided by 2?').should(eq([7]))
+  end
+
+  it('takes a string with two equations and returns each answer') do
+    calculator('What is 4 plus 2? What is 8 times 3?').should(eq([6, 24]))
   end
 end
