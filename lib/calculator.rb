28631e0646aedfb9e1196b5a79698d13594fd476
Meghan Lindsley & Aoede Pando <student@epicodus.com>
2014-07-28 10:53:56 -0700

    fourth commit, takes a division word equation and returns the answer.

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -2,12 +2,13 @@ def calculator(string)
   string.gsub! 'plus', '+'
   string.gsub! 'minus', '-'
   string.gsub! 'times', '*'
+  string.gsub! 'divided by', '/'
   array = string.split('')
   new_string = array.slice(8..-2).join('')
   answer = eval(new_string)
   p answer
 end
 
-calculator('What is 10 times 3?')
+calculator('What is 12 divided by 3?')
 
 
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -13,4 +13,8 @@ describe('calculator') do
   it('takes a multiplication word equation and returns the answer') do
     calculator('What is 3 times 4?').should(eq(12))
   end
+
+  it('takes a division word equation and returns the answer') do
+    calculator('What is 12 divided by 3?').should(eq(4))
+  end
 end
